09043cbfd1911e73e70875cb7501e5a2b7b3a146
Lionel Faber <lionel1704@gmail.com>
2020-02-04 16:54:20 +0530

    feat/auth: introduce backward compatibility
    - introduce backward compatibility with the last version of
    safe-authenticator

--- safe_authenticator/src/config.rs
+++ safe_authenticator/src/config.rs
@@ -20,7 +20,7 @@ use safe_core::fry;
 use safe_core::ipc::req::AppExchangeInfo;
 use safe_core::ipc::IpcError;
 use safe_core::{Client, CoreError, FutureExt};
-use safe_nd::{EntryError, Error as SndError, MDataSeqEntryActions};
+use safe_nd::{AppPermissions, EntryError, Error as SndError, MDataSeqEntryActions};
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use std::collections::{HashMap, VecDeque};
 use tiny_keccak::sha3_256;
@@ -38,6 +38,8 @@ pub struct AppInfo {
     pub info: AppExchangeInfo,
     /// Application keys
     pub keys: AppKeys,
+    /// Application permissions (Unused, kept for backwards compatibility)
+    pub perms: AppPermissions,
 }
 
 /// Config file key under which the list of registered apps is stored.
